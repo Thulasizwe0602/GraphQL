1553ee9472d359b82a30502345bed8fcc75ebd77
Mdletshe <s.mdletshe@marval.co.uk>
2019-12-01 19:29:47 +0200

    Fixed models

--- models/user.js
+++ models/user.js
@@ -10,7 +10,7 @@ const userSchema = new Schema({
         type: String,
         required : true
     },
-    emailAdress: {
+    emailAddress: {
         type: String,
         required : true
     },
--- models/userType.js
+++ models/userType.js
@@ -1,2 +1,11 @@
-const mongoose = request('mongoose');
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+
+const userTypeSchema = new Schema({
+    userTypeName: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('userType', userTypeSchema);
