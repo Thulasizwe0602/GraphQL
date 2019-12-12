71cf2c124b9a66c363341642ce5f712696073a66
Mdletshe <s.mdletshe@marval.co.uk>
2019-12-12 17:24:11 +0200

    removed links.

--- my-app/src/components/topNavigation.js
+++ my-app/src/components/topNavigation.js
@@ -31,27 +31,27 @@ class TopNavigation extends Component {
                             <MDBNavLink to="#">Home</MDBNavLink>
                         </MDBNavItem>
                         <MDBNavItem>
-                            <a rel="noopener noreferrer" className="nav-link Ripple-parent" href="https://mdbootstrap.com/docs/react/" target="_blank">About MDB</a>
+                            <a rel="noopener noreferrer" className="nav-link Ripple-parent" href="" target="_blank">About MDB</a>
                         </MDBNavItem>
                         <MDBNavItem>
-                            <a rel="noopener noreferrer" className="nav-link Ripple-parent" href="https://mdbootstrap.com/docs/react/getting-started/download/" target="_blank">Free download</a>
+                            <a rel="noopener noreferrer" className="nav-link Ripple-parent" href="" target="_blank">Free download</a>
                         </MDBNavItem>
                         <MDBNavItem>
-                            <a rel="noopener noreferrer"  className="nav-link Ripple-parent" href="https://mdbootstrap.com/bootstrap-tutorial/" target="_blank">Free tutorials</a>
+                            <a rel="noopener noreferrer"  className="nav-link Ripple-parent" href="" target="_blank">Free tutorials</a>
                         </MDBNavItem>
                     </MDBNavbarNav>
                     <MDBNavbarNav right>
                         <MDBNavItem>
-                            <a className="nav-link navbar-link" rel="noopener noreferrer" target="_blank" href="https://pl-pl.facebook.com/mdbootstrap/"><MDBIcon fab icon="facebook" /></a>
+                            <a className="nav-link navbar-link" rel="noopener noreferrer" target="_blank" href=""><MDBIcon fab icon="facebook" /></a>
                         </MDBNavItem>
                         <MDBNavItem>
-                            <a className="nav-link navbar-link" rel="noopener noreferrer" target="_blank" href="https://twitter.com/mdbootstrap"><MDBIcon fab icon="twitter" /></a>
+                            <a className="nav-link navbar-link" rel="noopener noreferrer" target="_blank" href=""><MDBIcon fab icon="twitter" /></a>
                         </MDBNavItem>
                         <MDBNavItem>
-                            <a className="border border-light rounded mr-1 nav-link Ripple-parent" rel="noopener noreferrer" href="https://github.com/mdbootstrap/React-Bootstrap-with-Material-Design" target="_blank"><MDBIcon fab icon="github" className="mr-2"/>MDB GitHub</a>
+                            <a className="border border-light rounded mr-1 nav-link Ripple-parent" rel="noopener noreferrer" href="" target="_blank"><MDBIcon fab icon="github" className="mr-2"/>MDB GitHub</a>
                         </MDBNavItem>
                         <MDBNavItem>
-                            <a className="border border-light rounded mr-1 nav-link Ripple-parent" rel="noopener noreferrer" href="https://mdbootstrap.com/products/react-ui-kit/" target="_blank"><MDBIcon fab icon="github" className="mr-2"/>Go Pro</a>
+                            <a className="border border-light rounded mr-1 nav-link Ripple-parent" rel="noopener noreferrer" href="" target="_blank"><MDBIcon fab icon="github" className="mr-2"/>Go Pro</a>
                         </MDBNavItem>
                     </MDBNavbarNav>
                 </MDBCollapse>
