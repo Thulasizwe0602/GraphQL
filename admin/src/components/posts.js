7a7cbb6f110d891327586fa8269ede0a21a7075e
Mdletshe <s.mdletshe@marval.co.uk>
2020-01-26 18:52:34 +0200

    Support Mobile Devices.

--- admin/src/components/posts.js
+++ admin/src/components/posts.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import { List, Datagrid, TextField, ReferenceField, EditButton, 
-         SimpleForm, ReferenceInput, SelectInput, TextInput, 
-         Edit, Create, Filter } from 'react-admin';
-
+import { useMediaQuery } from '@material-ui/core';
+import {
+    List, Datagrid, TextField, ReferenceField, EditButton,
+    SimpleForm, ReferenceInput, SelectInput, TextInput,
+    Edit, Create, Filter, SimpleList
+} from 'react-admin';
 
 
 const PostFilter = (props) => (
@@ -14,28 +16,40 @@ const PostFilter = (props) => (
     </Filter>
 );
 
-export const PostList = props => (
-    <List filters={<PostFilter />} {...props}>
-        <Datagrid>
-            <TextField source="id" />
-            <ReferenceField source="userId" reference="users">
-                <TextField source="name" />
-            </ReferenceField>
-            <TextField source="title" />
-            <EditButton />
-        </Datagrid>
-    </List>
-);
+export const PostList = (props) => {
+    const isSmall = useMediaQuery(theme => theme.breakpoints.down('sm'));
+    return (
+        <List filters={<PostFilter />} {...props}>
+            {isSmall ? (
+                <SimpleList
+                    primaryText={record => record.title}
+                    secondaryText={record => `${record.views} views`}
+                    tertiaryText={record => new Date(record.published_at).toLocaleDateString()}
+                />
+            ) : (
+                    <Datagrid>
+                        <TextField source="id" />
+                        <ReferenceField source="userId" reference="users">
+                            <TextField source="name" />
+                        </ReferenceField>
+                        <TextField source="title" />
+                        <EditButton />
+                    </Datagrid>
+            )}
+        </List>
+    );
+
+};
 
 export const PostEdit = props => (
     <Edit {...props}>
         <SimpleForm>
-         <TextInput disabled source="id" />
+            <TextInput disabled source="id" />
             <ReferenceInput source="userId" reference="users">
-              <SelectInput optionText="name" />
+                <SelectInput optionText="name" />
             </ReferenceInput>
             <TextInput source="title" />
-           <TextInput multiline source="body" />
+            <TextInput multiline source="body" />
         </SimpleForm>
     </Edit>
 );
